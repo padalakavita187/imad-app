00091d0a46e8b173fa42a7a15f8f9e7b7dcf09be
padalakavita187 <30648347+padalakavita187@users.noreply.github.com>
2017-08-11 10:23:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,41 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+function createTemplate(data){
+    var title = data.title;
+    var date = data.date;
+    var heading = data.heading;
+    var content = data.content;
+    var htmlTemplate = `
+    <html>
+        <head>
+            <title>
+            $(title)
+            </title>
+            <link href="/ui/style.css" rel="stylesheet" />
+        </head>
+        <body>
+            <div class="center">
+                <div>
+                    <a href = "/">Home Page</a>
+                </div>
+                <hr/>
+                <h3>
+                    $(heading)
+                </h3>
+                 <div>
+                    $(date)
+                </div>   
+                <div>
+                    $(content)
+                </div>
+            </div>
+        </body>
+    </html>
+    `;
+    return htmlTemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -18,7 +53,7 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+res.send(createTemplate(articleOne));
 });
 
 // Do not change port, otherwise your app won't run on IMAD servers
