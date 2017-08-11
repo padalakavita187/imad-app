4a78276a7363d75a255b2e16f8b26cfe29dca8eb
padalakavita187 <30648347+padalakavita187@users.noreply.github.com>
2017-08-11 10:31:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ function createTemplate(data){
     <html>
         <head>
             <title>
-            $(title)
+            ${title}
             </title>
             <link href="/ui/style.css" rel="stylesheet" />
         </head>
@@ -39,13 +39,13 @@ function createTemplate(data){
                 </div>
                 <hr/>
                 <h3>
-                    $(heading)
+                    ${heading}
                 </h3>
                  <div>
-                    $(date)
+                    ${date}
                 </div>   
                 <div>
-                    $(content)
+                    ${content}
                 </div>
             </div>
         </body>
