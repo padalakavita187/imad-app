621c76c6a3570f1eb3e46b6cf6c2740be934afa7
padalakavita187 <30648347+padalakavita187@users.noreply.github.com>
2017-08-17 13:52:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -42,6 +42,7 @@ console.log('Loaded!');
               request.onreadystatechange = function(){
                   
                 if(request.readyState === XMLHttpRequest.DONE){
+                    
                      if(request.status === 200){
                         var names = request.responseText;
                         names = JSON.parse(names);
@@ -59,7 +60,7 @@ console.log('Loaded!');
               //make the request
               var nameInput = document.getElementById('name');
               var name = nameInput.value;
-              request.open('GET','http://padalakavita187.imad.hasura-app.io/submit-name?name='+name,true);
+              request.open('GET','http://padalakavita187.imad.hasura-app.io/submit-name?name=' + name,true);
               request.send(null);
             };
             
