a153478803cdde32bedfcb36c907dc6900eb4d8a
padalakavita187 <30648347+padalakavita187@users.noreply.github.com>
2017-08-17 12:55:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -69,7 +69,7 @@ console.log('Loaded!');
             var name = nameInput.value;
             var submit = document.getElementById('submit_btn');
             submit.onclick = function(){
-              var names = ['name1','name2','name3'];
+              var names = ['Kavita','Rani','Padala'];
               var list = '';
               for(var i = 0; i<names.length; i++){
                   list += '<li>' + names[i] + '</li>';
