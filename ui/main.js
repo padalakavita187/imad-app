ab30a7960fd38363642c0e539bffcb56eb90f77c
padalakavita187 <30648347+padalakavita187@users.noreply.github.com>
2017-08-17 13:04:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -49,7 +49,7 @@ console.log('Loaded!');
                         for (var i = 0; i<names.length;i++){
                             list += '<li>'+names[i]+'</li>';
                             
-                    }
+                        }
                     var ul = document.getElementById('namelist');
                     ul.innerHTML = list;
                     }
