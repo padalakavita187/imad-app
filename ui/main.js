40d5cf908018a595ca4d5673b9a068eb32b2a5c4
padalakavita187 <30648347+padalakavita187@users.noreply.github.com>
2017-08-17 13:03:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -35,21 +35,20 @@ console.log('Loaded!');
               
               //create a request object
               
-              var request = new XMLHTTPRequest();
+              var request = new XMLHttpRequest();
               
               //capture the response and store it in a variable
               
               request.onreadystatechange = function(){
                   
-                if(request.readyState === XMLHttpRequest.Done){
-                     if(request.State === 200){
-                    
-                    var names = request.responseText;
-                    names = JSON.parse(names);
-                    var list = '';
-                    for (var i = 0; i<names.length;i++){
-                        list += '<li>'+names[i]+'</li>';
-                        
+                if(request.readyState === XMLHttpRequest.DONE){
+                     if(request.status === 200){
+                        var names = request.responseText;
+                        names = JSON.parse(names);
+                        var list = '';
+                        for (var i = 0; i<names.length;i++){
+                            list += '<li>'+names[i]+'</li>';
+                            
                     }
                     var ul = document.getElementById('namelist');
                     ul.innerHTML = list;
