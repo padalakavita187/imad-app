75472bd48b183302fd5ace47e394c2322bd95398
padalakavita187 <30648347+padalakavita187@users.noreply.github.com>
2017-08-17 12:54:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -28,7 +28,7 @@ console.log('Loaded!');
                
             };
             
-            //submit text java script
+        /*    //submit text java script
             
             var submit = document.getElementById('submit_btn');
             submit.onclick = function(){
@@ -63,3 +63,39 @@ console.log('Loaded!');
               request.open('get','http://padalakavita187.imad.hasura-app.io/submit-name?name='+name,true);
               request.send(null);
             };
+            */
+            
+            var nameInput = document.getElementById('name');
+            var name = nameInput.value;
+            var submit = document.getElementById('submit_btn');
+            submit.onclick = function(){
+              var names = ['name1','name2','name3'];
+              var list = '';
+              for(var i = 0; i<names.length; i++){
+                  list += '<li>' + names[i] + '</li>';
+              }
+              var ul = document.getElementById('namelist');
+              ul.innerHTML = list;
+                
+            };
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
